764c776b7349841ba37e33d9de5704df5acf7618
Abhi <59563596+a1-s2@users.noreply.github.com>
2020-07-22 17:10:39 +0530

    Update testserver.sh

--- testserver.sh
+++ testserver.sh
@@ -6,7 +6,7 @@ if test $ip == 31136
 then
 echo ""
 else
-status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.102:$ip)
+status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.100:$ip)
 echo $status_code
 if test $status_code == 200
 then 
